166780b094f8d06a59b1a1feb0b5d6a859053eb1
LT28400 <LT28400@PCBLC9893>
2018-03-23 19:08:10 +0100

    BIA.Net.DataTable 1.7.5

---
+++ src/BIA.Net.Common/Helpers/OpenXmlExcelHelper.cs
@@ -0,0 +1,68 @@
+﻿namespace BIA.Net.Common.Helpers
+{
+    using DocumentFormat.OpenXml;
+    using DocumentFormat.OpenXml.Packaging;
+    using DocumentFormat.OpenXml.Spreadsheet;
+    using System.Collections.Generic;
+    using System.IO;
+
+    /// <summary>
+    /// OpenXml Excel Helper
+    /// </summary>
+    public static class OpenXmlExcelHelper
+    {
+        /// <summary>
+        /// return a new instance of the CellValue (CellValues.String)
+        /// </summary>
+        /// <param name="text">text value</param>
+        /// <returns>excel cell</returns>
+        public static Cell GetCell(string text)
+        {
+            return new Cell
+            {
+                CellValue = new CellValue(text),
+                DataType = CellValues.String
+            };
+        }
+
+        /// <summary>
+        /// Creates the work book.
+        /// </summary>
+        /// <param name="rows">The rows.</param>
+        /// <returns>File Byte Array</returns>
+        public static byte[] CreateWorkBook(List<Row> rows)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (SpreadsheetDocument spreadSheet = SpreadsheetDocument.Create(memoryStream, SpreadsheetDocumentType.Workbook, true))
+                {
+                    // Add a WorkbookPart to the document.
+                    WorkbookPart workbookpart = spreadSheet.AddWorkbookPart();
+                    workbookpart.Workbook = new Workbook();
+
+                    // Add a WorksheetPart to the WorkbookPart.
+                    WorksheetPart worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
+
+                    worksheetPart.Worksheet = new Worksheet(new SheetData(rows));
+
+                    // Add Sheets to the Workbook.
+                    Sheets sheets = spreadSheet.WorkbookPart.Workbook.AppendChild(new Sheets());
+
+                    string sheetName = "sheet1";
+
+                    // Append a new worksheet and associate it with the workbook.
+                    Sheet sheet = new Sheet()
+                    {
+                        Id = spreadSheet.WorkbookPart.GetIdOfPart(worksheetPart),
+                        SheetId = 1,
+                        Name = sheetName,
+                    };
+
+                    sheets.Append(sheet);
+                }
+
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
---
+++ src/BIA.Net.DataTable.DTO/DTO/DataTableColumnDTO.cs
@@ -0,0 +1,41 @@
+﻿namespace BIA.Net.DataTable.DTO
+{
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// DataTable Column
+    /// </summary>
+    [DataContract(Name = "Column")]
+    public class DataTableColumnDTO
+    {
+        /// <summary>
+        /// Gets or sets Data
+        /// </summary>
+        [DataMember(Name = "data")]
+        public string Data { get; set; }
+
+        /// <summary>
+        /// Gets or sets name of column
+        /// </summary>
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this column is searchable.
+        /// </summary>
+        [DataMember(Name = "searchable")]
+        public bool Searchable { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this column is sortable
+        /// </summary>
+        [DataMember(Name = "orderable")]
+        public bool Orderable { get; set; }
+
+        /// <summary>
+        /// Gets or sets object <see cref="DataTableSearchDTO"/>
+        /// </summary>
+        [DataMember(Name = "search")]
+        public DataTableSearchDTO Search { get; set; }
+    }
+}
