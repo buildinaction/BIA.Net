dec5f8a210a434a516e7285639a0f7c731482099
Aurélien Zulian <aurelien.zulian@safrangroup.com>
2019-01-18 13:33:12 +0100

    Adding Resources

---
+++ src/BIA.Net.Common/ADLanguageHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Safran.Net.Common
+{
+    public class ADLanguageHelper
+    {
+
+        /// <summary>
+        /// Retrurn the language to use in application for the user and default en-US
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public static string GetADLanguageOrDefault(string userlanguage)
+        {
+            string languageCode = GetADLanguage(userlanguage);
+            if (languageCode == null)
+            {
+                return "en-US";
+            }
+            return languageCode;
+        }
+
+        /// <summary>
+        /// Retrurn the language to use in application for the user
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public static string GetADLanguage(string userlanguage)
+        {
+            string languageCode = null;
+            if (!string.IsNullOrEmpty(userlanguage))
+            {
+                switch (userlanguage)
+                {
+                    case "FR":
+                    case "MA":
+                        languageCode = "fr-FR";
+                        break;
+                    case "ES":
+                    case "MX":
+                        languageCode = "es-ES";
+                        break;
+                    case "GB":
+                        languageCode = "en-GB";
+                        break;
+                    case "US":
+                        languageCode = "en-US";
+                        break;
+                    case "DE":
+                        languageCode = "de-DE";
+                        break;
+                }
+            }
+            return languageCode;
+        }
+    }
+}
--- src/BIA.Net.Common/LanguageInfo.cs
+++ src/BIA.Net.Common/LanguageInfo.cs
@@ -1,34 +1,45 @@
-﻿// <copyright file="LanguageInfo.cs" company="BIA.NET">
-// Copyright (c) BIA.NET. All rights reserved.
+﻿// <copyright file="LanguageInfo.cs" company="Safran">
+// Copyright (c) Safran. All rights reserved.
 // </copyright>
 
-namespace BIA.Net.Common
+namespace Safran.Common
 {
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
 
     /// <summary>
-    /// The language information.
+    /// Class used for the multi-language change
     /// </summary>
     public class LanguageInfo
     {
         /// <summary>
-        /// The dictionnary of all languages available.
+        /// Dictionary containing the languages the application is translatable into
         /// </summary>
-        public static readonly IEnumerable<LanguageInfo> AllLanguageInfos = BIASettingsReader.BIANetSection.Language.GetApplicationLanguages();
+        public static readonly Dictionary<string, LanguageInfo> LanguageInfoDictionary = new Dictionary<string, LanguageInfo>
+        {
+            { "DeutschDE", new LanguageInfo { Name = "Deutsch DE", ShortName = "DE", Code = "de-DE" } },
+            { "EnglishUS", new LanguageInfo { Name = "English US", ShortName = "EN US", Code = "en-US" } },
+            { "EnglishGB", new LanguageInfo { Name = "English GB", ShortName = "EN GB", Code = "en-GB" } },
+            { "Français", new LanguageInfo { Name = "Français", ShortName = "FR", Code = "fr-FR" } },
+            { "Español", new LanguageInfo { Name = "Español", ShortName = "ES", Code = "es-ES" } }
+        };
 
         /// <summary>
-        /// Gets or sets the language code.
-        /// </summary>
-        public string Code { get; set; }
-
-        /// <summary>
-        /// Gets or sets the language name.
+        /// Gets or sets name of the language
         /// </summary>
         public string Name { get; set; }
 
         /// <summary>
-        /// Gets or sets the language short name.
+        /// Gets or sets shortname of the language that is displayed in the menu
         /// </summary>
         public string ShortName { get; set; }
+
+        /// <summary>
+        /// Gets or sets cultureInfo code of the language
+        /// </summary>
+        public string Code { get; set; }
     }
 }
