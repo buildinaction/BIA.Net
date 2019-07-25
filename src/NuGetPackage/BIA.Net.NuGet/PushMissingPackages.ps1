9c1cae665ae10bc919f0e9854b9198448ef84acf
Gwenael Fillon <gwenael.fillon@safrangroup.com>
2019-07-25 15:47:37 +0200

    BIA.Net.Design V2.0.1 icon management change

--- src/NuGetPackage/BIA.Net.NuGet/PushMissingPackages.ps1
+++ src/NuGetPackage/BIA.Net.NuGet/PushMissingPackages.ps1
@@ -2,6 +2,8 @@
 
 # .\nuget.exe sources Add -Name "SEP_Repository" -Source "https://tfsdm.eu.labinal.snecma/SAFRANEP%20Collection/_packaging/SEP_Repository/nuget/v3/index.json"
 #  .\PushMissingPackages.ps1 "D:\Sources\Innovation Factory\*\*\packages\*\*.nupkg" VSTS "SEP_Repository"
+#  .\PushMissingPackages.ps1 "D:\Sources\GitHub\BIA.Net\src\NuGetPackage\BIA.Net.NuGet\BIA.Net.Dialog.MVC\BIA.Net.Dialog.MVC.2.0.0.1.nupkg" VSTS "SEP_Repository"
+#  .\PushMissingPackages.ps1 "D:\Sources\GitHub\BIA.Net\src\NuGetPackage\BIA.Net.NuGet\BIA.Net.Design\BIA.Net.Design.2.0.1.nupkg" VSTS "SEP_Repository"
 
 $filesList = Get-ChildItem -Path $files
 $filesList | ForEach-Object{
