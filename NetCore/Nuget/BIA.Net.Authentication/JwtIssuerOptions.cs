93c46f21680445eb40e19dbba812f9c3260ee84e
Alexis FONTAINE <alexis.p.fontaine@gmail.com>
2020-04-03 15:51:06 +0200

    Add the ValidTime configurable

--- NetCore/Nuget/BIA.Net.Authentication/JwtIssuerOptions.cs
+++ NetCore/Nuget/BIA.Net.Authentication/JwtIssuerOptions.cs
@@ -14,18 +14,6 @@ namespace BIA.Net.Authentication
     public class JwtIssuerOptions
     {
         /// <summary>
-        /// Gets or sets the "iss" (Issuer) Claim - The "iss" (issuer) claim identifies the
-        /// principal that issued the JWT.
-        /// </summary>
-        public string Issuer { get; set; }
-
-        /// <summary>
-        /// Gets or sets the "sub" (Subject) Claim - The "sub" (subject) claim identifies the
-        /// principal that is the subject of the JWT.
-        /// </summary>
-        public string Subject { get; set; }
-
-        /// <summary>
         /// Gets or sets the "aud" (Audience) Claim - The "aud" (audience) claim identifies the
         /// recipients that the JWT is intended for.
         /// </summary>
@@ -38,21 +26,16 @@ namespace BIA.Net.Authentication
         public DateTime Expiration => this.IssuedAt.Add(this.ValidFor);
 
         /// <summary>
-        /// Gets or sets the "nbf" (Not Before) Claim - The "nbf" (not before) claim identifies the
-        /// time before which the JWT MUST NOT be accepted for processing.
-        /// </summary>
-        public DateTime NotBefore => DateTime.UtcNow;
-
-        /// <summary>
         /// Gets or sets the "iat" (Issued At) Claim - The "iat" (issued at) claim identifies the
         /// time at which the JWT was issued.
         /// </summary>
         public DateTime IssuedAt => DateTime.UtcNow;
 
         /// <summary>
-        /// Set the timespan the token will be valid for (default is 120 min).
+        /// Gets or sets the "iss" (Issuer) Claim - The "iss" (issuer) claim identifies the
+        /// principal that issued the JWT.
         /// </summary>
-        public TimeSpan ValidFor { get; set; } = TimeSpan.FromMinutes(120);
+        public string Issuer { get; set; }
 
         /// <summary>
         /// "jti" (JWT ID) Claim (default ID is a GUID)
@@ -61,8 +44,30 @@ namespace BIA.Net.Authentication
           () => Task.FromResult(Guid.NewGuid().ToString());
 
         /// <summary>
+        /// Gets or sets the "nbf" (Not Before) Claim - The "nbf" (not before) claim identifies the
+        /// time before which the JWT MUST NOT be accepted for processing.
+        /// </summary>
+        public DateTime NotBefore => DateTime.UtcNow;
+
+        /// <summary>
         /// The signing key to use when generating tokens.
         /// </summary>
         public SigningCredentials SigningCredentials { get; set; }
+
+        /// <summary>
+        /// Gets or sets the "sub" (Subject) Claim - The "sub" (subject) claim identifies the
+        /// principal that is the subject of the JWT.
+        /// </summary>
+        public string Subject { get; set; }
+
+        /// <summary>
+        /// Set the timespan the token will be valid for (default is 120 min).
+        /// </summary>
+        public TimeSpan ValidFor => TimeSpan.FromMinutes(this.ValidTime == 0 ? 120 : this.ValidTime);
+
+        /// <summary>
+        /// The time the token will be valid.
+        /// </summary>
+        public int ValidTime { get; set; }
     }
 }
